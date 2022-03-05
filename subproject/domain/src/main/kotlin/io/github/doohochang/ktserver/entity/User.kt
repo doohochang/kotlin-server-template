cc3b60e66cdd525282e56de84b1aad8093eab0a1
Dooho Chang <doohochang@gmail.com>
2022-03-05 19:06:51 +0900

    Implement User.validateName

--- subproject/domain/src/main/kotlin/io/github/doohochang/ktserver/entity/User.kt
+++ subproject/domain/src/main/kotlin/io/github/doohochang/ktserver/entity/User.kt
@@ -1,5 +1,7 @@
 package io.github.doohochang.ktserver.entity
 
+import arrow.core.Either
+
 /**
  * Represents a user whose [name] consists only of alphanumeric characters.
  *
@@ -8,4 +10,12 @@ package io.github.doohochang.ktserver.entity
 data class User(
     val id: String,
     val name: String
-)
+) {
+    companion object {
+        fun validateName(name: String): Either<String, Unit> =
+            if (name.filterNot { it.isDigit() || it in 'a'..'z' || it in 'A'..'Z' }.isNotEmpty())
+                Either.Left("A user name should consists of alphanumeric characters.")
+            else
+                Either.Right(Unit)
+    }
+}
